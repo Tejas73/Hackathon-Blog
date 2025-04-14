e3490f2e59fed8d66e31d5d14685357d192fbf82
Anuvrat <anuvratarya1504@gmail.com>
2025-04-14 10:54:33 +0530

    login

---
+++ blog-app/src/pages/Login.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Login() {
+  return (
+    <div>
+      <h1 className='page-header'>Login</h1>
+      <div className='row'>
+        <div className='col'></div>
+        <div className='col'>
+          <div>
+            <div className='mb-3'>
+              <label htmlFor=''>Email</label>
+              <input
+                type='email'
+                className='form-control'
+              />
+            </div>
+            <div className='mb-3'>
+              <label htmlFor=''>Password</label>
+              <input
+                type='password'
+                className='form-control'
+              />
+            </div>
+            <div className='mb-3'>
+              <div className='mb-3'>
+                Don't have an account yet?
+                <Link to='/register'>Register here</Link>
+              </div>
+              <button className='btn btn-success'>Sign in</button>
+
+                
+              
+              <button className='btn btn-success'>Sign up</button>
+            </div>
+          </div>
+        </div>
+        <div className='col'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
